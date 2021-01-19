080a6e6eb6d78e16908c353ebfa1605cd5b67088
Austin Casteel <austin.casteel@gmail.com>
2021-01-19 15:18:21 -0600

    initial push

---
+++ __init__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import json
+from naomi import plugin
+
+
+class DadJokes(plugin.SpeechHandlerPlugin):
+    def intents(self):
+        return {
+            'VersionIntent': {
+                'locale': {
+                    'en-US': {
+                        'templates': [
+                            'CHECK VERSION',
+                            'WHAT VERSION ARE YOU RUNNING',
+                            'IS AUTO UPDATE ENABLED',
+                            'IS AUTO UPDATE DISABLED',
+                            'IS AUTO UPDATE ENABLED OR DISABLED',
+                            'IS AUTO UPDATE DISABLED OR ENABLED',
+                            'ENABLE AUTO UPDATE',
+                            'DISABLE AUTO UPDATE'
+                        ]
+                    }
+                },
+                'action': self.handle
+            }
+        }
+
+    def handle(self, intent, mic):
+        transcript = intent['input']
+
+        with open('~/.config/naomi/configs/.naomi_options.json' 'r') as optionsFile:
+            data = json.load(optionsFile)
+
+            if "VERSION" in transcript:
+                mic.say(data['version'])
+
+            if "IS AUTO UPDATE" in transcript:
+                auto = data['auto_update']
+                if "false" in auto:
+                    mic.say("AUTO UPDATE IS DISABLED")
+                else "true" in auto:
+                    mic.say("AUTO UPDATE IS ENABLED")
+        
+            if "ENABLE AUTO UPDATE" in transcript:
+                auto = data['auto_update']
+                if "false" in auto:
+                    data['auto_update'] = "true"
+                    with open('~/.config/naomi/configs/.naomi_options.json', "w") as optionsFile:
+                        json.dump(data, optionsFile)
+                    mic.say("ENABLED AUTO UPDATE")
+                else "true" in auto:
+                    mic.say("AUTO UPDATE IS ALREADY ENABLED")
+
+            if "DISABLE AUTO UPDATE" in transcript:
+                auto = data['auto_update']
+                if "true" in auto:
+                    data['auto_update'] = "false"
+                    with open('~/.config/naomi/configs/.naomi_options.json', "w") as optionsFile:
+                        json.dump(data, optionsFile)
+                    mic.say("DISABLED AUTO UPDATE")
+                else "false" in auto:
+                    mic.say("AUTO UPDATE IS ALREADY DISABLED")
---
+++ readme.md
@@ -0,0 +1,31 @@
+---
+id: version-checker
+label: Version Checker
+title: Version Checker - Speechhandler
+type: speechhandlers
+description: "Lets Naomi tell you what version it is running"
+logo: images/plugins/smile.svg
+source: https://github.com/austincasteel/versionchecker/blob/master/readme.md
+meta:
+  - property: og:title
+    content: "Version Checker - Speechhandler"
+  - property: og:description
+    content: "Lets Naomi tell you what version it is running"
+---
+
+# Version Checker - Speechhandler
+
+Naomi can tell you what version it is running as well as if you have auto updates enabled or not.
+
+Intents:
+
+- Check Version
+- What Version Are You Running?
+- Is Auto Update Enabled?
+- Is Auto Update Disabled?
+- Is Auto Update Enabled or Disabled?
+- Is Auto Update Disabled or Enabled?
+- Enable Auto Update
+- Disable Auto Update
+
+<EditPageLink/>
